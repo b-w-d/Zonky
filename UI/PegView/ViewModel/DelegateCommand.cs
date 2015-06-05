7ed49d78ca8c1661a3d591859cb86f0fc8627a75
b-w-d <briandah@BRIANDAH-T7600.redmond.corp.microsoft.com>
2015-06-05 15:20:27 -0700

    Adding First MVVM pattern classes

---
+++ UI/PegView/ViewModel/DelegateCommand.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PegView.ViewModel
+{
+    using System.Windows.Input;
+
+    class DelegateCommand  : ICommand
+    {
+        #region Fields
+
+        readonly Action<object> _action;
+        readonly Predicate<object> _canExecute;        
+
+        #endregion // Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _action = execute;
+            _canExecute = canExecute;           
+        }
+
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
+
+        #endregion // ICommand Members
+    }
+
+}
---
+++ UI/PegView/ViewModel/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PegView.ViewModel
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Base for all ViewModel classes. 
+    /// This handles the INotifyPropertyChanged interface, allowing updates to be passed to the view.
+    /// 
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
