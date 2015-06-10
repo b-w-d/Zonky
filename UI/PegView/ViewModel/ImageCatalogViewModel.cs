5a542258d47b0e0ac0356e0b32f4977b4873a95d
bwdahlquist <bwdahlquist@gmail.com>
2015-06-09 21:26:07 -0700

    First ViewModel for CatalogItem

---
+++ UI/PegView/ViewModel/ImageCatalogViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+using ImageCatalog;
+
+namespace PegView.ViewModel
+{
+    public class ImageCatalogViewModel : ViewModelBase
+    {
+        protected ObservableCollection<CatalogItemBase> items;
+
+        public ImageCatalogViewModel()
+        {
+            this.items = new ObservableCollection<CatalogItemBase>();
+        }
+
+        public ObservableCollection<CatalogItemBase> Catalog
+        {
+            get
+            {
+                return this.items;
+            }
+        }
+
+        public void AddItem(CatalogItemBase itemToAdd)
+        {
+            this.items.Add(itemToAdd);
+            this.RaisePropertyChangedEvent("Catalog");
+        }
+
+        public void RemoveItem(CatalogItemBase itemToRemove)
+        {
+            if(this.items.Contains(itemToRemove))
+            {
+                this.items.Remove(itemToRemove);
+            }
+
+            this.RaisePropertyChangedEvent("Catalog");
+        }
+    }
+}
